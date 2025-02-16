45c69deb9d259d56c1879e5950256b20ec63aa10
Leonardho Rante S <leonardho81@gmail.com>
2025-02-16 22:48:04 +0700

    [DS] Descriptive Statistic - Find Mean, Modus, Median

--- descriptive_statistics.go
+++ descriptive_statistics.go
@@ -5,10 +5,12 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
 )
 
-// Still onprpgress
+// Desciptive Statistic - Count the most words in founded in every cell defined
 func countWordFrequencies(records [][]string, columnIndex int) map[string]int {
 	wordCounts := make(map[string]int)
 
@@ -33,6 +35,44 @@ func findMostFrequentWord(wordCounts map[string]int) (string, int) {
 	}
 
 	return mostFrequentWord, maxCount
+}
+
+// Descriptive Statistic - Find Mean, Modus, Median
+func findMean(values []float64) float64 {
+	var sum float64
+	for _, value := range values {
+		sum += value
+	}
+	return sum / float64(len(values))
+}
+
+func findMode(values []float64) []float64 {
+	frequencyMap := make(map[float64]int)
+	var maxCount int
+
+	for _, value := range values {
+		frequencyMap[value]++
+		if frequencyMap[value] > maxCount {
+			maxCount = frequencyMap[value]
+		}
+	}
+
+	var modes []float64
+	for value, count := range frequencyMap {
+		if count == maxCount {
+			modes = append(modes, value)
+		}
+	}
+
+	return modes
+}
+func findMedian(values []float64) float64 {
+	sort.Float64s(values)
+	n := len(values)
+	if n%2 == 0 {
+		return (values[n/2-1] + values[n/2]) / 2
+	}
+	return values[n/2]
 }
 
 func main() {
@@ -73,5 +113,40 @@ func main() {
 		mostFrequentWord, maxCount := findMostFrequentWord(wordCounts)
 
 		fmt.Printf("For column '%s', most frequent word: '%s' with %d occurrences\n", colName, mostFrequentWord, maxCount)
+	}
+
+	// Descriptive Statistic - Find Mean, Modus, Median
+	target_col_2 := []string{"Current Employee Rating", "Engagement Score", "Satisfaction Score", "Work-Life Balance Score", "Training Duration(Days)"}
+	for _, colName := range target_col_2 {
+		columnIndex := -1
+		for i, header := range headers {
+			if header == colName {
+				columnIndex = i
+				break
+			}
+		}
+
+		if columnIndex == -1 {
+			fmt.Printf("Column '%s' not found in the CSV file\n", colName)
+			continue
+		}
+
+		var columnValues []float64
+		for _, record := range records[1:] {
+			if len(record) > columnIndex {
+				value := record[columnIndex]
+				if v, err := strconv.ParseFloat(value, 64); err == nil {
+					columnValues = append(columnValues, v)
+				}
+			}
+		}
+
+		fmt.Printf("\nFor column '%s'\n", colName)
+		mean := findMean(columnValues)
+		fmt.Printf("Mean: %.2f\n", mean)
+		mode := findMode(columnValues)
+		fmt.Printf("Mode: %v\n", mode)
+		median := findMedian(columnValues)
+		fmt.Printf("Median: %.2f\n", median)
 	}
 }
